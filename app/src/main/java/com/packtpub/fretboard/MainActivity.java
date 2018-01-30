1c36460db1106d395e6d6c8093f320b6fa574105
gallen <gallen.coding@gmail.com>
2018-01-30 18:05:33 +0000

    Fretboard : Added

---
+++ app/src/main/java/com/packtpub/fretboard/MainActivity.java
@@ -0,0 +1,42 @@
+package com.packtpub.fretboard;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends Activity implements View.OnClickListener
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button goButton = (Button)findViewById(R.id.goButton);
+        goButton.setOnClickListener(this);
+        TextView tuningTextView = (TextView)findViewById(R.id.tuningTextView);
+        TextView myNotesTextView = (TextView)findViewById(R.id.myNotesTextView);
+
+    }
+
+    @Override
+    public void onClick(View view){
+        if(view.getId() == R.id.goButton)
+        {
+            TextView tuningTextView = (TextView)findViewById(R.id.tuningTextView);
+            String tuningString = tuningTextView.getText().toString();
+            TextView myNotesTextView = (TextView)findViewById(R.id.myNotesTextView);
+            String chosenNotes = myNotesTextView.getText().toString();
+            Intent i;
+            i = new Intent(this,fretboardActivity.class);
+            startActivity(i);
+
+        }
+
+    }
+}
---
+++ app/src/main/java/com/packtpub/fretboard/fretboardActivity.java
@@ -0,0 +1,13 @@
+package com.packtpub.fretboard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class fretboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fretboard);
+    }
+}
