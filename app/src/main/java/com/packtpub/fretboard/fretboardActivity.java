63647c4cf0af7e0964e576a42b46aee2f30a5fa4
GAllen425 <gallen.coding@gmail.com>
2018-02-03 17:46:54 +0000

    Laptop Update: converted strings passed into fretboardActivity to array list

--- app/src/main/java/com/packtpub/fretboard/fretboardActivity.java
+++ app/src/main/java/com/packtpub/fretboard/fretboardActivity.java
@@ -2,11 +2,14 @@ package com.packtpub.fretboard;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Color;
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import android.graphics.Bitmap;
@@ -25,13 +28,24 @@ public class fretboardActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        ImageView fretboardImageView = (ImageView) findViewById(R.id.fretboardImageView);
-        Bitmap fretboardBitmap = Bitmap.createBitmap(400, 800, Bitmap.Config.ARGB_8888);
-        Paint myStringPaint = new Paint();
-        Canvas fretboardCanvas = new Canvas(fretboardBitmap);
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fretboard);
+
+        ScrollView fretboardScrollView = (ScrollView) findViewById(R.id.fretboardScrollView);
+        ImageView fretboardImageView = (ImageView) findViewById(R.id.fretboardImageView);
+
+        int width = Resources.getSystem().getDisplayMetrics().widthPixels;
+        int height = Resources.getSystem().getDisplayMetrics().heightPixels;
+        //int width = fretboardScrollView.getWidth();
+        //int height = fretboardScrollView.getHeight();
+
+        Log.d("onCreate", "width: " + width);
+        Log.d("onCreate", "height: " + height);
+
+
+        Bitmap bitmap = Bitmap.createBitmap(100,300, Bitmap.Config.ARGB_8888);
+        Canvas fretboardCanvas = new Canvas(bitmap);
 
         Intent i = getIntent();
         Bundle extrasBundle = i.getExtras();
@@ -68,12 +82,43 @@ public class fretboardActivity extends Activity {
         }
         // TODO decide whether to pass canvas, bitmap or imageview?
         // TODO write function that draws strings then focus on frets after
-        drawFretboard(tuningNoteArrayList, chosenNotesArrayList);
+        drawFretboard(tuningNoteArrayList, chosenNotesArrayList,bitmap,fretboardCanvas);
+        Bitmap fretboardBitmap = Bitmap.createScaledBitmap(bitmap,width,height,true);
+        fretboardImageView.setImageBitmap(fretboardBitmap);
     }
 
-    public void drawFretboard (ArrayList tuningNoteArrayList,  ArrayList chosenNotesArrayList)
+    public void drawFretboard (ArrayList tuningNoteArrayList,  ArrayList chosenNotesArrayList, Bitmap bitmap, Canvas fretboardCanvas)
     {
+        Paint myPaint = new Paint();
+        myPaint.setColor(Color.BLACK);
 
+        int screenWidth = bitmap.getWidth();
+        int screenHeight = bitmap.getHeight();
+
+        float stringWidth = screenWidth/64;
+        float startOfScreenX = screenWidth/8;
+
+        int numberOfStrings = tuningNoteArrayList.size();
+        int numberOfChosenNotes = chosenNotesArrayList.size();
+
+        float stringX[] = new float[numberOfStrings];
+        Log.d("numberOfString", "" + numberOfStrings);
+        Log.d("screenWidth", "" + screenWidth);
+
+        float stringSeparation = 0;
+        for(int i=0; i < numberOfStrings ; i++) {
+            stringSeparation = (float)(1./(numberOfStrings-1)*(1-2*startOfScreenX/screenWidth)*screenWidth);
+            stringX[i] = (startOfScreenX + i*stringSeparation - stringWidth/2);
+            Log.d("stringSeparation", "" + stringSeparation);
+            Log.d("stringX", "String[" +i+ "]" + stringX[i]);
+            fretboardCanvas.drawRect(stringX[i], screenHeight, stringX[i] + stringWidth, 0, myPaint);
+        }
+        int numberOfFrets = 12;
+        float stringY[] = new float[numberOfFrets];
+        for(int i=0; i<numberOfFrets; i++){
+            stringY[i] = i*(screenHeight/(numberOfFrets-1));
+            fretboardCanvas.drawRect(startOfScreenX, stringY[i],stringWidth + stringX[numberOfStrings-1], stringY[i] + stringWidth,myPaint);
+        }
     }
 
 }
